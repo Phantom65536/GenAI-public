66d371d1394b6fc74a2e72298661949f858b5951
Flw5469 <tonyfong12345@gmail.com>
2024-03-31 06:26:06 +0800

    combined input page and writing page into a higher-level InputWritingPage

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,31 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import InputPage from './InputPage.js'
-import WritingPage from './WritingPage.js'
-import { useState } from 'react';
+import InputWritingPage from './InputWritingPage.js';
+
+const mockData={
+  Title: "Fun things in Canada",
+  Topic: "Top 20 ways to leave Canada",
+  Pairs: [["pt1", "ela1"], ["pt2", "ela2"], ["pt3", "ela3"]],
+  Ending: "In conclusion, Canada is a good place to leave"
+}
 
 function App() {
-  const [open,setOpen]=useState(true)
-  const [writing,setWriting]=useState(false)
-  const mockPage = {
-    Title: "Fun things in Canada",
-    Topic: "Top 20 ways to leave Canada",
-    Pairs: [["pt1", "ela1"], ["pt2", "ela2"], ["pt3", "ela3"]],
-    Ending: "In conclusion, Canada is a good place to leave",
-    open: ()=>{setOpen(true);},
-    close: ()=>{setOpen(false);},
-    startWriting: ()=>{setWriting(true)}
-  };
   return (
     <>
-      {open ? (
-        <div>
-          <InputPage {...mockPage} />
-          {writing ? <WritingPage {...mockPage} /> : null}
-        </div>
-      ) : <div></div>}
+    <div>
+    <InputWritingPage {...mockData}/>
+    </div>
     </>
-  );
-}
+    );
+  }
 
 export default App;
---
+++ frontend/my-app/src/InputWritingPage.js
@@ -0,0 +1,23 @@
+import InputPage from './InputPage.js'
+import WritingPage from './WritingPage.js'
+import { useState } from 'react';
+function InputWritingPage(props){
+  const [open,setOpen]=useState(true)
+  const [writing,setWriting]=useState(false)
+  const object = {
+    ...props,
+    open: ()=>{setOpen(true);},
+    close: ()=>{setOpen(false);},
+    startWriting: ()=>{setWriting(true)}
+  };
+  return(
+  <>
+      {open ? (
+        <div>
+          <InputPage {...object} />
+          {writing ? <WritingPage {...object} /> : null}
+        </div>
+      ) : <div></div>}
+  </>)
+}
+export default InputWritingPage
