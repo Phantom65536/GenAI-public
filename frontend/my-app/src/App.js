e44ad1f9c2e735c2b18c10853a05b707c8410fbc
Flw5469 <tonyfong12345@gmail.com>
2024-03-31 18:08:55 +0800

    update

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -8,13 +8,11 @@ const mockData={
   Topic: "Top 20 ways to leave Canada",
   Pairs: [["pt1", "ela1"], ["pt2", "ela2"], ["pt3", "ela3"]],
   Ending: "In conclusion, Canada is a good place to leave",
-  writing:""
+  writing:"This is a good essay.\nThis is a good essay.\nThis is a good essay.\nThis is a good essay.\n"
 }
 
-const mockAssignmentList={names:["Peter","Mark","Tom","Jerry","Marie"],mockData}
-
 function App() {
-  const ele = <div>{/*<StudentListPage {...mockAssignmentList}/>*/ <InputWritingPage {...mockData}/> }</div>;
+  const ele = <div>{/*<StudentListPage {...mockAssignmentList}/> <InputWritingPage {...mockData}/>*/<FeedbackPage {...mockData}/> }</div>;
 
   return (
     <>
