8dca861ec1bd50ea70fce0a64515da6bb5953f21
Flw5469 <tonyfong12345@gmail.com>
2024-03-31 15:36:27 +0800

    integrated input page API

--- frontend/my-app/src/FeedbackPage.js
+++ frontend/my-app/src/FeedbackPage.js
@@ -1,4 +1,25 @@
 function FeedbackPage(props){
+  useEffect(() => {/*send the request for the the description */
+    const object = new FormData()
+    object.append('topic',process.env.REACT_APP_TOPIC.replace("***", '\n'))
+    console.log(object.get("topic"))
+    console.log(JSON.stringify(object))
+    console.log(process.env.REACT_APP_URL+"table")
+    fetch(process.env.REACT_APP_URL+"table",{
+      method:'POST',
+      body: object
+    }).then(response => response.json()).then(
+      result => {
+        console.log(result);
+        Description.TopicDesc=result.T
+        Description.ImportantDetailDesc=result.I
+        Description.DetailedExaminationDesc=result.D
+        Description.EndingDesc=result.E
+      }).catch(error => {
+      // Handle any errors
+      console.error(error)
+    })
+  })
 
   return (<>
   <div>
